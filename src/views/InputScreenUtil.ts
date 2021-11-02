f4f472ecc95851405c9290bf273d58e89f78fc55
Joris <chinook25@gmail.com>
2021-11-02 10:46:17 +0100

    don't use errors for control flow

--- src/views/InputScreenUtil.ts
+++ src/views/InputScreenUtil.ts
@@ -6,44 +6,42 @@ export const submitPseudonymRegistration = async (
 ): Promise<{pseudonym: string; isDuplicate: boolean}> => {
   let pseudonym = '';
   let isDuplicate = false;
-  const postOptions = {body: JSON.stringify({OriginalId: originalId})};
 
   try {
-    await api
-      .post(`/api/data/PseudoId_PseudonymRegistration`, postOptions)
-      .then(
-        async (response: ApiResponse) => {
-          if (response.status === 201) {
-            pseudonym = await getNewPseudonym(originalId);
-          } else {
-            throw new Error(`Unexpected status code ${response.status}`);
-          }
-        },
-        async (error: Response) => {
-          if (error.status === 400) {
-            const result = await checkForDuplicateId(originalId);
-            pseudonym = result.pseudonym;
-            isDuplicate = result.isDuplicate;
-          } else {
-            throw new Error(
-              `${error.statusText} (statuscode: ${error.status}). Please contact a system administrator`
-            );
-          }
-        }
-      );
+    pseudonym = await getPseudonym(originalId);
+    isDuplicate = Boolean(pseudonym);
+
+    if (!isDuplicate) {
+      pseudonym = await createPseudonym(originalId);
+    }
   } catch (error: any) {
     await Promise.reject(error.toString());
   }
+
   return {pseudonym, isDuplicate};
 };
 
-const getNewPseudonym = async (originalId: string): Promise<string> => {
-  let newPseudonym = '';
-  await api
+const createPseudonym = async (originalId: string) => {
+  const postOptions = {body: JSON.stringify({OriginalId: originalId})};
+  return await api
+    .post(`/api/data/PseudoId_PseudonymRegistration`, postOptions)
+    .then(async (response: ApiResponse) => {
+      if (response.status === 201) {
+        return await getPseudonym(originalId);
+      } else {
+        throw new Error(
+          `Could not create pseudonym. Unexpected status code ${response.status}`
+        );
+      }
+    });
+};
+
+const getPseudonym = async (originalId: string): Promise<string> => {
+  return await api
     .get(`/api/data/PseudoId_PseudonymRegistration?q=OriginalId==${originalId}`)
     .then(
       (response: ApiResponse) => {
-        newPseudonym = response.items[0].data.id;
+        return response.items.length ? response.items[0].data.id : '';
       },
       (error: ApiResponse) => {
         throw new Error(
@@ -51,32 +49,6 @@ const getNewPseudonym = async (originalId: string): Promise<string> => {
         );
       }
     );
-  return newPseudonym;
-};
-
-const checkForDuplicateId = async (
-  originalId: string
-): Promise<{pseudonym: string; isDuplicate: boolean}> => {
-  let isDuplicate = false;
-  let pseudonym = '';
-  await api
-    .get(`/api/data/PseudoId_PseudonymRegistration?q=OriginalId==${originalId}`)
-    .then(
-      (response: ApiResponse): void => {
-        if (response.items.length) {
-          pseudonym = response.items[0].data.id;
-          isDuplicate = true;
-        } else {
-          throw new Error(`Please contact a system administrator`);
-        }
-      },
-      (error: ApiResponse): void => {
-        throw new Error(
-          `${error.statusText} (statuscode: ${error.status}). Please contact a system administrator`
-        );
-      }
-    );
-  return {pseudonym, isDuplicate};
 };
 
 type ApiResponse = Response & {items: {data: {id: string}}[]};
